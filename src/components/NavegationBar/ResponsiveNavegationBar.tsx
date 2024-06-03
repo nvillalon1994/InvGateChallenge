a9523ba034d12d33cfec7c23b7ddbc385611dc04
nvillalon1994 <natalius_94@hotmail.com>
2024-06-03 15:52:43 -0300

    responsive navbar

--- src/components/NavegationBar/ResponsiveNavegationBar.tsx
+++ src/components/NavegationBar/ResponsiveNavegationBar.tsx
@@ -15,11 +15,11 @@ export default function ResponsiveNavegationBar({
       } transition-all duration-500  w-full h-full z-20 p-5`}
     >
       <div className=" relative w-full flex flex-col gap-8 ">
-        <div className="flex flex-col text-2xl w-full items-center">
+        <div className="flex flex-col text-2xl w-full  items-center">
           {links.map((link) => (
             <div
               key={link.id}
-              className="flex flex-col justify-center border-y w-full h-full py-2"
+              className="flex flex-col justify-center border-y w-full h-full py-4"
             >
               <div className=" flex items-center justify-center gap-3 cursor-pointer ">
                 <span className="  text-base lg:text-lg font-medium  text-[#687382] leading-6 indent-0.5">
@@ -32,11 +32,11 @@ export default function ResponsiveNavegationBar({
               </div>
             </div>
           ))}
-          <span className="sm:hidden text-base lg:text-lg cursor-pointer font-medium py-2 text-[#687382] leading-6 indent-0.5">
+          <span className="sm:hidden text-base lg:text-lg cursor-pointer font-medium py-4 text-[#687382] leading-6 indent-0.5">
             Contact us
           </span>
 
-          <button className="sm:hidden bg-[#08A1FF] w-full text-base lg:text-lg px-5 py-2 text-white cursor-pointer rounded-md">
+          <button className="sm:hidden bg-[#08A1FF] w-full text-base lg:text-lg px-5 py-2 my-2 text-white cursor-pointer rounded-md">
             Subscribe
           </button>
         </div>
